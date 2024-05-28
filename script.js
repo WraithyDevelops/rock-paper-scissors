8abc524d285edadffab9a954b594dcfb616be7c3
WraithyDevelops <156721715+WraithyDevelops@users.noreply.github.com>
2024-05-28 21:37:37 +0200

    Implementation of the human players input logic
    Created the core input logic for the human player.

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 console.log("Hellow world!");
 
-let choice;
-let result;
+let computerChoice;
+let computerResult;
+let humanChoice;
+let humanResult;
 
 function getComputerChoice() {
-    choice = Math.floor(Math.random() * 3);  
-    if(choice === 0) {
-     result = "Rock";
-    } else if (choice === 1) {
-     result = "Paper";
+    computerChoice = Math.floor(Math.random() * 3);  
+    if(computerChoice === 0) {
+     computerResult = "Rock";
+    } else if (computerChoice === 1) {
+     computerResult = "Paper";
     } else {
-     result = "Scissors";
-    } 
-    
-    return result;
+     computerResult = "Scissors";
+    }     
+    return computerResult;
+}
+
+function getHumanChoice() {
+    humanChoice = prompt("Rock, Paper or Scissors?");
+    if (humanChoice === "Rock") {
+        humanResult = "Rock";
+    } else if (humanChoice === "Paper") {
+        humanResult = "Paper";
+    } else if (humanChoice === "Scissors") {
+        humanResult = "Scissors";
+    } else {
+        humanResult =  alert("Incorrect choice, please use only one the three choices available!");
+    }
+    return humanResult;
 }
 
 
 
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
