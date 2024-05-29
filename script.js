8cf8a1ba28d12a52e91224c0a4be277cbd439fd0
WraithyDevelops <156721715+WraithyDevelops@users.noreply.github.com>
2024-05-29 10:31:05 +0200

    playRound logic implementation
    Player and the computer can initialize a round, which returns the result
    of the winner player, and their choice.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let computerChoice;
 let computerResult;
 let humanChoice;
 let humanResult;
+let computerScore = 0;
+let humanScore = 0;
+let roundResult;
 
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3);  
@@ -18,21 +21,42 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    humanChoice = prompt("Rock, Paper or Scissors?");
-    if (humanChoice === "Rock") {
+    humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    if (humanChoice === "rock") {
         humanResult = "Rock";
-    } else if (humanChoice === "Paper") {
+    } else if (humanChoice === "paper") {
         humanResult = "Paper";
-    } else if (humanChoice === "Scissors") {
+    } else if (humanChoice === "scissors") {
         humanResult = "Scissors";
     } else {
         humanResult =  alert("Incorrect choice, please use only one the three choices available!");
     }
     return humanResult;
 }
+ let x = getHumanChoice();
+ let y = getComputerChoice();
+
+function playRound(x,y) {
+    if (humanResult === "Rock" && computerResult === "Rock" || humanResult === "Paper" && computerResult === "Paper" || humanResult === "Scissors" && computerResult === "Scissors" ) {
+        roundResult = "Draw";
+    } else if (humanResult === "Rock" && computerResult === "Scissors" || humanResult === "Paper" && computerResult === "Rock" || humanResult === "Scissors" && computerResult === "Paper") {
+        roundResult = "Human won!";
+    } else {
+        roundResult = "Computer won!";
+    }
+    return console.log("Human choice:" + humanResult + " " + "Computers choice: " + computerResult + " == the Result is " + roundResult);
+}
+
+function checkWinner(roundResult) {
+    if(roundResult === "Human won!") {
+        humanScore = 1;
+    } else {
+        computerScore = 1;
+    }
+    return humanScore, computerScore;
+}
+
+playRound();
+console.log(checkWinner());
 
 
-
-
-console.log(getComputerChoice());
-console.log(getHumanChoice());
